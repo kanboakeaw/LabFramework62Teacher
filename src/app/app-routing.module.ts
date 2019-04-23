4cda9c5a189b5150d56ed8af092e860aeaf652ac
weerayut1996 <weerayut.itsd@gmail.com>
2019-04-24 00:19:20 +0700

    frontend_login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { AppComponent } from "./app.component";
 import { LoginComponent } from "./login/login.component";
 
 const routes: Routes = [
-  { path: "", component: AppComponent },
+  { path: "", redirectTo: "/login", pathMatch: "full" }, //redirect หน้าแรกไป Login
   { path: "login", component: LoginComponent }
 ];
 
