6f325b8bf5bbde6f70ca02c5a2ad969f8f583070
yoskamol <yoskamol@gmail.com>
2019-04-25 14:44:02 +0700

    connect_fontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "/login", pathMatch: "full" }, //redirect หน้าแรกไป Login
   { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent }
+  { path: "register", component: RegisterComponent },
+  { path: "home", component: HomeComponent }
+
+
 ];
 
 @NgModule({
