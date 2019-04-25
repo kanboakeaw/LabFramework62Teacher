1f6fc6a407f2e2f045331deca7b78066ed7968c5
vongbhan <vongbhan.soi@gmail.com>
2019-04-25 15:19:00 -0700

    frontendconnect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,19 @@ import { Routes, RouterModule } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: "", redirectTo: "/login", pathMatch: "full" }, //redirect หน้าแรกไป Login
-  { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent }
+ { path: "", redirectTo: "/login", pathMatch: "full" }, //redirect หน้าแรกไป Login
+ { path: "login", component: LoginComponent },
+ { path: "register", component: RegisterComponent },
+ { path: "home", component: HomeComponent }
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+ imports: [RouterModule.forRoot(routes)],
+ exports: [RouterModule]
 })
 export class AppRoutingModule {}
+
