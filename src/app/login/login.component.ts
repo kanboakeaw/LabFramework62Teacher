844e20dc2fd57d709a927cb3f18d07a8acf5cbf9
sumeten52 <sumeten52@gmail.com>
2019-04-26 12:17:10 +0700

    backend_database

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,23 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { Route } from '@angular/compiler/src/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+ selector: 'app-login',
+ templateUrl: './login.component.html',
+ styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+ constructor (private router:Router) { }
 
-  constructor (private router:Router) {}
+ ngOnInit() {
+ }
 
-  ngOnInit() {
-  }
-
-  onSubmit(){
-    this.router.navigate(["/home"]);
-    
-    }
-    
+ onSubmit(){
+   this.router.navigate(["/home"]);
+  
+   }
+  
 
 }
