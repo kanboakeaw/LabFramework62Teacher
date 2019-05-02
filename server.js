f1a643fc99e539560f8c64a792dcecc4cea7f932
kanboakeaw <kanboakeaw@gmail.com>
2019-05-02 10:10:50 +0700

    Test deploy to Heroku3

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory 
-app.use(express.static('./dist/{{LabFramework62Teacher}}'));
+app.use(express.static('./dist/LabFramework62Teacher'));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, '/dist/{{LabFramework62Teacher}}/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/LabFramework62Teacher/index.html'));
 });
 
 // Start the app by listening on the default Heroku port 
